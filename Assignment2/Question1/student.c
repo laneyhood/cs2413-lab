49e2532eea123ac421b12be44351a5ef2eec9098
laneyhood <laney.hood405@gmail.com>
2026-03-09 15:07:26 -0500

    Assignment2: Question 1 & Question 2 complete

--- Assignment2/Question1/student.c
+++ Assignment2/Question1/student.c
@@ -27,6 +27,30 @@ struct TreeNode {
 struct TreeNode* bstSearch(struct TreeNode* root, int target) {
     // TODO: implement
     // Hint: Use the BST property to decide whether to go left or right.
+
+    // Edge cases
+
+    // Base case: return null if root is null, return root if target has been found
+    if(root == NULL || root->val == target)
+    {
+        return root;
+    }
+    // else if(root->left == NULL && root->right == NULL)
+    // {
+    //     // If you have reached a leaf node without finding target, return NULL
+    //     return NULL;
+    // }
+    // Recursively search left subtree
+    else if(target < root->val)
+    {
+        return bstSearch(root->left, target);
+    }
+    // Recursively search right subtree
+    else
+    {
+        return bstSearch(root->right, target);
+    }
+
     (void)root;
     (void)target;
     return NULL;
--- Assignment2/Question2/student.c
+++ Assignment2/Question2/student.c
@@ -21,6 +21,8 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <limits.h>
+#include <stdlib.h>
 
 struct TreeNode {
     int val;
@@ -28,11 +30,73 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+int height(struct TreeNode* root, long min, long max)
+{
+    // Base case: leaf nodes have height 0
+    if(root == NULL)
+    {
+        // 1 + max(-1, -1) = height 0
+        return -1;
+    }
+
+    // Return -999 if BST violation
+    if(root->val <= min || root->val >= max)
+    {
+        return -999;
+    }
+
+    // Recursion
+    int left, right;
+    left = height(root->left, min, (long) root->val);
+    if(left == -999) return -999;
+    right = height(root->right, (long) root->val, max);
+    if(right == -999) return -999;
+
+    // Check for unbalance
+    if(abs(left - right) > 1)
+    {
+        return -999;
+    }
+    
+    int max_height = left > right ? left : right;
+
+    return 1 + max_height;
+
+    //return -1;
+}
+
 bool isAVL(struct TreeNode* root) {
     // TODO: implement
     // Hint: One common O(n) approach:
     // - Use a recursive helper that returns the subtree height,
     //   and returns -1 if subtree is invalid (BST violation or unbalanced).
+
+    // If root is null, tree is empty and is therefore balanced
+    if(root == NULL)
+    {
+        return true;
+    }
+
+    // Calculate heights of left and right subtrees
+    int left_height = height(root->left, LONG_MIN, (long) root->val);
+    int right_height = height(root->right, (long) root->val, LONG_MAX);
+
+    // Check if height function returns large negative for BST violation or unbalanced tree
+    if(left_height < -1 || right_height < -1)
+    {
+        return false;
+    }
+    // If entire tree is unbalanced, return false
+    else if(abs(left_height - right_height) > 1)
+    {
+        return false;
+    }
+    // Otherwise, tree is an AVL
+    else
+    {
+        return true;
+    }
+
     (void)root;
     return false;
 }
